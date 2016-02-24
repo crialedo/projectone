2b8058c5e2992c07345f922298d71dc4dc48c1c5
Cristina <crialedo@gmail.com>
2016-02-24 18:59:16 +0100

    A change in the operations.

--- operations.py
+++ operations.py
@@ -2,6 +2,7 @@
             
 def suma(x,y): return x+y
 def resta(x,y): return x-y
-def multiplicacion(x,y): return x*y
-def division(x,y): return x/y
+#def multiplicacion(x,y): return x*y
+#def division(x,y): return x/y
+
 
