da6c3aca69bc6f1d0c20a5c413d1501583f721ee
Cristina <crialedo@gmail.com>
2016-02-24 19:19:06 +0100

    Changed, again, the operation

--- operations.py
+++ operations.py
@@ -1,7 +1,8 @@
 ##MÓDULOS
-            
+
+#Nueva prueba            
 def suma(x,y): return x+y
-def resta(x,y): return x-y
+#def resta(x,y): return x-y
 #def multiplicacion(x,y): return x*y
 #def division(x,y): return x/y
 
